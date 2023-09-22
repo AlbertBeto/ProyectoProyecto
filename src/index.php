88916fe916e5e2d060a6ce2a2a531188d5f7699a
Albert <albertweb08@gmail.com>
2023-09-22 19:21:04 +0200

    Inicio

---
+++ src/index.php
@@ -0,0 +1,13 @@
+<?php include("templates/header.php");?>
+<div class="container">
+    <a href="#">
+        <div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="static/images/proyecto1.jpeg" alt="Proyecto 1">
+            <div class="card-body">
+                <h5 class="card-title">Proyecto 1</h5>
+                <p class="card-text">Descripción del proyecto 1.</p>
+            </div>
+        </div>
+    </a>
+</div>
+<?php include("templates/footer.php");?>
---
+++ src/proyecto.php
@@ -0,0 +1,18 @@
+<?php include("templates/header.php"); ?>
+    <div class="container">
+        <h2>Título de muestra</h2>
+        <h4><a href="#">Año</a></h4>
+        <span>Categorías: </span>
+        <a href="#"><button class="btn btn-sm btn-default">Categoría 1</button></a>
+        <br> <br>
+        <div class="row">
+            <div class="col-sm">
+                <img src="static/images/proyecto1.jpeg" alt="Proyecto 1" class="img-responsive">
+                <br>
+            </div>
+            <div class="col-sm">
+                Descripción
+            </div>
+        </div>
+    </div>
+<?php include("templates/footer.php"); ?>
