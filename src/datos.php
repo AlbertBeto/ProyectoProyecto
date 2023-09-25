f35599b40064ef95793fb0d490f49570589993b5
Albert <albertweb08@gmail.com>
2023-09-25 17:55:20 +0200

    Creo Datos php

---
+++ src/datos.php
@@ -0,0 +1,30 @@
+$proyectos = [
+	[
+		"clave" => "proyecto1",
+		"titulo" => "Título proyecto 1",
+		"descripcion" => "Descripción proyecto 1",
+		"imagen" => "static/images/proyecto1.jpg",
+		//"categoria" => [],
+	],
+
+	[
+		"clave" => "proyecto2",
+		"titulo" => "Título proyecto 2",
+		"descripcion" => "Descripción proyecto 2",
+		"imagen" => "static/images/proyecto1.jpg"
+		//"categoria" => [],
+	],
+
+	[
+		"clave" => "proyecto3",
+		"titulo" => "Título proyecto 3",
+		"descripcion" => "Descripción proyecto 3",
+		"imagen" => "static/images/proyecto1.jpg"
+		//"categoria" => [],
+	],
+
+];
+
+$categorias = [
+	"php" => "PHP", "js" => "JavaScript", "py" => "Python", "mysql" => "MySQL"
+];
