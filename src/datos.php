0224181f16993a612799de5ba1838c4676b21b04
albert <albertweb08@gmail.com>
2023-10-03 09:40:11 +0200

    3.3.a Incorporo imagenes. Y modifico Arrays

--- src/datos.php
+++ src/datos.php
@@ -1,38 +1,45 @@
 <?php
 $proyectos = [
+	//3.3.a Modifico lo arrays para hacerlos "Reales". De momento la descripción la lleno de Lorem ipsu. 
 	[
-		"clave" => "proyecto1",
-		"titulo" => "Título proyecto 1",
-		"descripcion" => "Descripción proyecto 1",
-		"imagen" => "static/images/proyecto1.jpeg"//,
+		"clave" => "Comisaria",
+		"titulo" => "Comisaria social",
+		"descripcion" => "Lorem fistrum amatomaa sexuarl benemeritaar ese hombree la caidita pecador ese hombree por la gloria de mi madre me cago en tus muelas. Sexuarl hasta luego Lucas ese que llega caballo blanco caballo negroorl fistro al ataquerl va usté muy cargadoo. Ahorarr a wan te va a hasé pupitaa ahorarr pecador sexuarl.",
+		"imagen" => "static/images/comisaria.jpg"//,
 		//"categorias" => []
 	],
 
 	[
-		"clave" => "proyecto3",
-		"titulo" => "Título proyecto 3",
-		"descripcion" => "Descripción proyecto 3",
-		"imagen" => "static/images/proyecto1.jpeg"
+		"clave" => "trenza",
+		"titulo" => "Peluqueria Lola",
+		"descripcion" => "Lorem fistrum amatomaa sexuarl benemeritaar ese hombree la caidita pecador ese hombree por la gloria de mi madre me cago en tus muelas. Sexuarl hasta luego Lucas ese que llega caballo blanco caballo negroorl fistro al ataquerl va usté muy cargadoo. Ahorarr a wan te va a hasé pupitaa ahorarr pecador sexuarl.",
+		"imagen" => "static/images/trenza.jpg"
 		//"categorias" => []
 	],
 
 	[
-		"clave" => "proyecto6",
-		"titulo" => "Título proyecto 6",
-		"descripcion" => "Descripción proyecto 6",
-		"imagen" => "static/images/proyecto1.jpeg"
+		"clave" => "calamar",
+		"titulo" => "Pescaderia",
+		"descripcion" => "Lorem fistrum amatomaa sexuarl benemeritaar ese hombree la caidita pecador ese hombree por la gloria de mi madre me cago en tus muelas. Sexuarl hasta luego Lucas ese que llega caballo blanco caballo negroorl fistro al ataquerl va usté muy cargadoo. Ahorarr a wan te va a hasé pupitaa ahorarr pecador sexuarl.",
+		"imagen" => "static/images/calamar.jpg"
 		//"categorias" => []
 	],
 
 	[
-		"clave" => "proyecto2",
-		"titulo" => "Título proyecto 2",
-		"descripcion" => "Descripción proyecto 2",
-		"imagen" => ""
+		"clave" => "tenis",
+		"titulo" => "Club deportivo",
+		"descripcion" => "Lorem fistrum amatomaa sexuarl benemeritaar ese hombree la caidita pecador ese hombree por la gloria de mi madre me cago en tus muelas. Sexuarl hasta luego Lucas ese que llega caballo blanco caballo negroorl fistro al ataquerl va usté muy cargadoo. Ahorarr a wan te va a hasé pupitaa ahorarr pecador sexuarl.",
+		"imagen" => "static/images/tenis.jpg"
 		//"categorias" => []
 	],
 
-	
+	[
+		"clave" => "corso",
+		"titulo" => "Una de Piratas",
+		"descripcion" => "Lorem fistrum amatomaa sexuarl benemeritaar ese hombree la caidita pecador ese hombree por la gloria de mi madre me cago en tus muelas. Sexuarl hasta luego Lucas ese que llega caballo blanco caballo negroorl fistro al ataquerl va usté muy cargadoo. Ahorarr a wan te va a hasé pupitaa ahorarr pecador sexuarl.",
+		"imagen" => "static/images/pirata.jpg"
+		//"categorias" => []
+	],
 
 ];
 
