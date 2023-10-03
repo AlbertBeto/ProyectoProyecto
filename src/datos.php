c8cd8156d88cbb5a475b4a78e1f9b0933c5b7fcf
albert <albertweb08@gmail.com>
2023-10-03 09:44:59 +0200

    3.3.b Incluir dos campos mas en los array

--- src/datos.php
+++ src/datos.php
@@ -1,44 +1,50 @@
 <?php
 $proyectos = [
 	//3.3.a Modifico lo arrays para hacerlos "Reales". De momento la descripción la lleno de Lorem ipsu. 
+	//3.3.b Incorpor los campos categorias, que es un array, y la fecha que para darle un valor temporal le incorporo el formato. 
 	[
 		"clave" => "Comisaria",
 		"titulo" => "Comisaria social",
 		"descripcion" => "Lorem fistrum amatomaa sexuarl benemeritaar ese hombree la caidita pecador ese hombree por la gloria de mi madre me cago en tus muelas. Sexuarl hasta luego Lucas ese que llega caballo blanco caballo negroorl fistro al ataquerl va usté muy cargadoo. Ahorarr a wan te va a hasé pupitaa ahorarr pecador sexuarl.",
-		"imagen" => "static/images/comisaria.jpg"//,
-		//"categorias" => []
+		"imagen" => "static/images/comisaria.jpg",
+		"categorias" => [],
+		"fecha" => "DD/MM/AAAA"
 	],
 
 	[
 		"clave" => "trenza",
 		"titulo" => "Peluqueria Lola",
 		"descripcion" => "Lorem fistrum amatomaa sexuarl benemeritaar ese hombree la caidita pecador ese hombree por la gloria de mi madre me cago en tus muelas. Sexuarl hasta luego Lucas ese que llega caballo blanco caballo negroorl fistro al ataquerl va usté muy cargadoo. Ahorarr a wan te va a hasé pupitaa ahorarr pecador sexuarl.",
-		"imagen" => "static/images/trenza.jpg"
-		//"categorias" => []
+		"imagen" => "static/images/trenza.jpg",
+		"categorias" => [],
+		"fecha" => "DD/MM/AAAA"
 	],
 
 	[
 		"clave" => "calamar",
 		"titulo" => "Pescaderia",
 		"descripcion" => "Lorem fistrum amatomaa sexuarl benemeritaar ese hombree la caidita pecador ese hombree por la gloria de mi madre me cago en tus muelas. Sexuarl hasta luego Lucas ese que llega caballo blanco caballo negroorl fistro al ataquerl va usté muy cargadoo. Ahorarr a wan te va a hasé pupitaa ahorarr pecador sexuarl.",
-		"imagen" => "static/images/calamar.jpg"
-		//"categorias" => []
+		"imagen" => "static/images/calamar.jpg",
+		"categorias" => [],
+		"fecha" => "DD/MM/AAAA"
 	],
 
 	[
 		"clave" => "tenis",
 		"titulo" => "Club deportivo",
 		"descripcion" => "Lorem fistrum amatomaa sexuarl benemeritaar ese hombree la caidita pecador ese hombree por la gloria de mi madre me cago en tus muelas. Sexuarl hasta luego Lucas ese que llega caballo blanco caballo negroorl fistro al ataquerl va usté muy cargadoo. Ahorarr a wan te va a hasé pupitaa ahorarr pecador sexuarl.",
-		"imagen" => "static/images/tenis.jpg"
-		//"categorias" => []
+		"imagen" => "static/images/tenis.jpg",
+		"categorias" => [],
+		"fecha" => "DD/MM/AAAA"
 	],
 
 	[
 		"clave" => "corso",
 		"titulo" => "Una de Piratas",
 		"descripcion" => "Lorem fistrum amatomaa sexuarl benemeritaar ese hombree la caidita pecador ese hombree por la gloria de mi madre me cago en tus muelas. Sexuarl hasta luego Lucas ese que llega caballo blanco caballo negroorl fistro al ataquerl va usté muy cargadoo. Ahorarr a wan te va a hasé pupitaa ahorarr pecador sexuarl.",
-		"imagen" => "static/images/pirata.jpg"
-		//"categorias" => []
+		"imagen" => "static/images/pirata.jpg",
+		"categorias" => [],
+		"fecha" => "DD/MM/AAAA"
 	],
 
 ];
